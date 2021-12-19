5a1045b88966dffe1e42c8a86d6cb470184c9f4c
seifgh <seifgh.dev@gmail.com>
2021-12-19 13:27:46 +0100

    Add client resource update errors

--- server/src/admin/resources/clientResource/actions.js
+++ server/src/admin/resources/clientResource/actions.js
@@ -1,7 +1,29 @@
+const { ValidationError } = require("admin-bro");
+const Client = require("../../../models/Client");
+const { DIRECTION_UPDATE_ERROR_MSG, PARENT_UPDATE_ERROR_MSG } = require("../configs/customErrors");
 const handleEncryptedPasswordErrors = require("../utils/handleEncryptedPasswordErrors");
 const handlePasswordEncryption = require("../utils/handlePasswordEncryption");
 const handleUniqueEmailError = require("../utils/handleUniqueEmailError");
 
+const preventParentAndDirectionUpdate = async (payload) => {
+    const clientToUpdate = await Client.findById(payload._id);
+    if (clientToUpdate) {
+        if (clientToUpdate.parent.toString() != payload.parent) {
+            throw new ValidationError({
+                parent: {
+                    message: PARENT_UPDATE_ERROR_MSG
+                }
+            })
+        }
+        if (clientToUpdate.direction != payload.direction) {
+            throw new ValidationError({
+                direction: {
+                    message: DIRECTION_UPDATE_ERROR_MSG
+                }
+            })
+        }
+    }
+}
 
 
 module.exports = {
@@ -13,6 +35,7 @@ module.exports = {
     },
     handleBeforeUpdateAction: async (req) => {
         if (req.method == "post") {
+            await preventParentAndDirectionUpdate(req.payload);
             handlePasswordEncryption(req.payload);
         }
         return req;
--- server/src/admin/resources/configs/customErrors.js
+++ server/src/admin/resources/configs/customErrors.js
@@ -1,4 +1,6 @@
 module.exports = {
     UNIQUE_EMAIL_ERROR_MSG: "L'enregistrement avec cet email existe déjà",
-    REQUIRED_ERROR_MSG: "Ce champ est requis"
+    REQUIRED_ERROR_MSG: "Ce champ est requis",
+    DIRECTION_UPDATE_ERROR_MSG: "Impossible de changer la direction, veuillez conserver l'ancien direction",
+    PARENT_UPDATE_ERROR_MSG: "Impossible de changer le parent, veuillez conserver l'ancien parent"
 }
