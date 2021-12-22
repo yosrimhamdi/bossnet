435ebb8566ca32111557d160b763bbf1f0039966
seifgh <seifgh.dev@gmail.com>
2021-12-22 03:19:07 +0100

    Update custom page url component

--- server/src/admin/resources/components/pageUrl.jsx
+++ server/src/admin/resources/components/pageUrl.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import { ValueGroup} from '@admin-bro/design-system'
+import React from "react";
+import { ValueGroup } from "@admin-bro/design-system";
 
-const CLIENT_ENDPOINT = "http://localhost:8080";
+const copyUrlField = ({ record: { id } }) => {
+  const pageURL = `/custom-pages/${id}`;
 
-const copyUrlField = ({ record : {id} }) => {
-    const pageURL = `${CLIENT_ENDPOINT}/custom-pages/${id}`;
+  return <ValueGroup value={pageURL} label="Lien de page" />;
+};
 
-    return (
-        <ValueGroup
-            value={pageURL}
-            label="Lien de page"
-        />
-    )
-  }
-  
-export default copyUrlField
-
+export default copyUrlField;
