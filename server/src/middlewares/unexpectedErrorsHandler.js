3fd55bcdd9548adca13f1e1985f1b2800a71acf5
seifgh <seifgh.dev@gmail.com>
2022-01-13 23:39:18 +0100

    Update unexpectedErrors handler

--- server/src/middlewares/unexpectedErrorsHandler.js
+++ server/src/middlewares/unexpectedErrorsHandler.js
@@ -1,6 +1,8 @@
 const { UNEXPECTED_ERROR_MSG } = require("../config/responseErrorsMsgs");
+const consola = require("consola");
 
 module.exports = (err, req, res, next) => {
+  consola.error(err);
   res.status(500);
   res.json({ error: UNEXPECTED_ERROR_MSG });
 };
