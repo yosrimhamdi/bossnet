038c7751620b54584b66210c3d00f590c4dd5e5f
seifgh <seifgh.dev@gmail.com>
2022-02-14 01:34:46 +0100

    Clean pm2 configs

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -12,30 +12,5 @@ module.exports = {
         NODE_ENV: "production",
       },
     },
-    // {
-    //   name: "bossnet-client",
-    //   script: "./client/node_modules/nuxt/bin/nuxt.js",
-    //   args: "dev",
-    //   env: {
-    //     NODE_ENV: "development",
-    //   },
-    //   env_production: {
-    //     NODE_ENV: "production",
-    //   },
-    // },
   ],
-
-  // deploy: {
-  //   production: {
-  //     user: "SSH_USERNAME",
-  //     host: "SSH_HOSTMACHINE",
-  //     ref: "origin/master",
-  //     repo: "GIT_REPOSITORY",
-  //     path: "DESTINATION_PATH",
-  //     "pre-deploy-local": "",
-  //     "post-deploy":
-  //       "npm install && pm2 reload ecosystem.config.js --env production",
-  //     "pre-setup": "",
-  //   },
-  // },
 };
