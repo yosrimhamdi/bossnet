a47655eaa20a6f180184137086670bd4756653c7
seifgh <seifgh.dev@gmail.com>
2022-02-21 02:49:38 +0100

    Update client pm2 configs

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: "bossnet-client",
       script: "./node_modules/nuxt/bin/nuxt.js",
       args: "start",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "development",
       },
